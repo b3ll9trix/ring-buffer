ba25efaf01eb46bce58898658c9371badafd7ee3
Reshma <reshmasuresh3005@gmail.com>
2022-01-18 21:56:43 +0100

    Thread1

--- src/Frame.h
+++ src/Frame.h
@@ -5,28 +5,47 @@
 #ifndef SRC_FRAME_H
 #define SRC_FRAME_H
 #include<ctime>
+typedef int frame_number_type;
 template<typename T>
-class Frame {
 
+class Frame {
     typedef T data_type;
-    typedef int frame_number_type;
     typedef char* timestamp_type;
 public:
     /// Constructor
-    explicit Frame(data_type data, frame_number_type frame_number)
-    :data(data), frame_number(frame_number) {
+    explicit Frame(data_type &data)
+    :frame_data(data), frame_number(frame_count++) {
         std::time_t t = std::time(nullptr);
-        timestamp = std::asctime(std::localtime(&t));
+        frame_timestamp = std::asctime(std::localtime(&t));
+
     }
     /// Destructor
     ~Frame() = default;
 
+    /// data
+    const data_type &data() const{
+        return frame_data;
+    }
+    /// timestamp
+    timestamp_type timestamp() const{
+        return frame_timestamp;
+    }
+    /// number - returns the frame number
+    frame_number_type number() const{
+        return frame_number;
+    }
+
 private:
-    data_type data;
+    static frame_number_type frame_count;
+    data_type frame_data;
     frame_number_type frame_number;
-    timestamp_type timestamp;
+    timestamp_type frame_timestamp;
 
 
 };
+
+template<typename  T>
+frame_number_type Frame<T>::frame_count = 0;
+
 
 #endif //SRC_FRAME_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,50 @@
 #include <iostream>
 #include "RingBuffer.h"
 #include "Frame.h"
+#include <thread>
+
+typedef int frame_rate_t;
+typedef std::string process_message_t;
+
+/// produce - produces frame every `frame_rate` seconds and pushes into the buffer infinitely
+[[noreturn]] void produce(frame_rate_t frame_rate, RingBuffer<Frame<int>> &buffer){
+    int i = 0;
+    while(true){
+        Frame<int> frame = Frame<int>(i);
+        buffer.push_back(frame);
+        i++;
+        std::this_thread::sleep_for(std::chrono::nanoseconds(frame_rate));
+    }
+}
+
+///// consume - consumes the oldest frame and processes it
+//[[noreturn]] void consume(RingBuffer<Frame<int>> &buffer){
+//    while(true){
+//        Frame<int> frame = buffer.pop_front();
+//
+//        //Dummy processing to see if the number is even or odd
+//        if (frame.data() % 2 ==0){
+//            process_message_t message = "Data at Frame:" + std::to_string(frame.number()) + "is Even. Timestamp:" + frame.timestamp();
+//            std::cout << message;
+//        } else {
+//            process_message_t message = "Data at Frame:" + std::to_string(frame.number()) + "is Odd. Timestamp:" + frame.timestamp();
+//            std::cout << message;
+//        }
+//    }
+//}
 
 
 int main() {
 
     //1. Initialization of the Buffer
-    RingBuffer<int> cf = RingBuffer<int>(4);
+    RingBuffer<Frame<int>> cb = RingBuffer<Frame<int>>(4);
 
     //2. Produce - Non blocking
-    Frame<int> frame = Frame<int>(4,5);
+    std::thread thread1(produce, 7, std::ref(cb));
 
     //3. Consumer
-    cf.push_back(9);
-    cf.push_back(10);
-    cf.push_back(11);
-    cf.push_back(12);
-    auto f = cf.readAll();
-    for (auto e: f) {
-        std::cout << e;
-        std::cout << "\n";
-    }
-    std::cout << "\n";
-    std::cout << cf[2] << "test";
-    std::cout << "\n";
-
-    std::cout << cf.pop_front();
-    std::cout << "\n";
-
-    std::cout << cf.back();
-
-    std::cout << "\n";
-    std::cout << cf.pop_front();
 
     return 0;
 
 }
+
